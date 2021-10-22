1b4a9db812263dac2546f0970b86c5717849f7aa
Aimanoshi <akinladeaima@gmail.com>
2021-10-22 19:01:01 +0200

    Fixed Api leak

--- www/js/Leesomgeving/leesomgeving_6.js
+++ www/js/Leesomgeving/leesomgeving_6.js
@@ -54,7 +54,7 @@
 
         fetch('https://api.airtable.com/v0/app9SiJUqIxbsSwyi/Leesomgeving_VRAGEN' + querystring, {
             headers: {
-                'Authorization': 'Bearer keybe7D1M6i7oaWj6',
+                'Authorization': process.env.apiKey,
             }
         })
             .then(response => response.json())
